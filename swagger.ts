a86f379c94d6c2aaa295d129904ecbd909548812
Trjunior <tarcisio.junior1406@gmail.com>
2024-09-06 12:27:33 -0300

    feat: create swagger.ts

---
+++ swagger.ts
@@ -0,0 +1,26 @@
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import { Express } from 'express';
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Documentation',
+      version: '1.0.0',
+      description: 'API endpoints documentation',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+  apis: ['src/presentation/routes/*.ts'], // Defina o caminho onde estão as rotas
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+export const setupSwagger = (app: Express) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+};
