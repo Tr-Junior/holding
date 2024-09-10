21ae701fcf2271d5f44f207f08dca7b33c3feb94
Trjunior <tarcisio.junior1406@gmail.com>
2024-09-10 18:17:43 -0300

    feat: create Holding.ts

---
+++ src/domain/entities/Holding.ts
@@ -0,0 +1,19 @@
+import { EntidadeOperacional } from "./EntidadeOperacional";
+
+export class Holding {
+  id: string;
+  nome: string;
+  dataCriacao: Date;
+  entidadesOperacionais: EntidadeOperacional[];
+
+  constructor(
+    id: string,
+    nome: string,
+    entidadesOperacionais: EntidadeOperacional[],
+  ) {
+    this.id = id;
+    this.nome = nome;
+    this.dataCriacao = new Date();
+    this.entidadesOperacionais = entidadesOperacionais;
+  }
+}
