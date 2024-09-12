3ab8a2843715848b3c606c1fc41a9b9bba4ddd1e
Trjunior <tarcisio.junior1406@gmail.com>
2024-09-12 17:50:06 -0300

    feat: create ContaPagarSchema.ts

---
+++ src/infra/db/mongoose/ContaPagarSchema.ts
@@ -0,0 +1,13 @@
+import { Schema, model } from 'mongoose';
+
+const ContaPagarSchema = new Schema({
+  descricao: { type: String, required: true },
+  codigoBarras: { type: String, required: true },
+  dataVencimento: { type: Date, required: true },
+  valor: { type: Number, required: true },
+  statusPagamento: { type: String, enum: ['PENDENTE', 'PAGO'], default: 'PENDENTE' },
+  boletoPdf: { type: String },
+  dataPagamento: { type: Date }
+});
+
+export const ContaPagarModel = model('ContaPagar', ContaPagarSchema);
