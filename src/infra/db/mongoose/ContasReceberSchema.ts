facd97efef0c2a60bbf4f27adc8e6c23c1b9d188
Trjunior <tarcisio.junior1406@gmail.com>
2024-09-12 17:50:02 -0300

    feat: create ContasReceberSchema.ts

---
+++ src/infra/db/mongoose/ContasReceberSchema.ts
@@ -0,0 +1,12 @@
+import { Schema, model } from 'mongoose';
+
+const ContaReceberSchema = new Schema({
+  holdingId: { type: Schema.Types.ObjectId, ref: 'Holding', required: true },
+  descricao: { type: String, required: true },
+  dataEmissao: { type: Date, required: true },
+  valor: { type: Number, required: true },
+  statusPagamento: { type: String, enum: ['PENDENTE', 'PAGO'], default: 'PENDENTE' },
+  dataRecebimento: { type: Date }
+});
+
+export const ContaReceberModel = model('ContaReceber', ContaReceberSchema);
