d5f51949f0b1eb16c5dfc95623b770106383dba9
Trjunior <tarcisio.junior1406@gmail.com>
2024-09-18 18:30:16 -0300

    update RelatorioSchema.ts

--- src/infra/db/mongoose/RelatorioSchema.ts
+++ src/infra/db/mongoose/RelatorioSchema.ts
@@ -2,12 +2,14 @@ import { Schema, model } from 'mongoose';
 
 const RelatorioSchema = new Schema({
   holdingId: { type: Schema.Types.ObjectId, ref: 'Holding', required: true },
-  contasReceber: [{
-    type: Schema.Types.ObjectId,
-    ref: 'ContaReceber',
-    required: true
-  }],
-  dataGeracao: { type: Date, default: Date.now }
+  contasReceber: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'ContaReceber',
+      required: true,
+    },
+  ],
+  dataGeracao: { type: Date, default: Date.now },
 });
 
 export const RelatorioModel = model('Relatorio', RelatorioSchema);
