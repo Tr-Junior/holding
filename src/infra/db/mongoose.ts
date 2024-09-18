604bdf258dca66202cf2a75a4f16d64519021a90
Trjunior <tarcisio.junior1406@gmail.com>
2024-09-18 18:35:06 -0300

    update mongoose.ts

--- src/infra/db/mongoose.ts
+++ src/infra/db/mongoose.ts
@@ -1,9 +1,11 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect('', {
-    });
+    await mongoose.connect(process.env.MONGO_URI || '', {});
     console.log('Conectado ao MongoDB com sucesso.');
   } catch (error) {
     console.error('Erro ao conectar ao MongoDB:', error);
