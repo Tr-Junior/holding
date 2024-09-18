874d6b25824c17822216303eefd6c01e4a872cf2
Trjunior <tarcisio.junior1406@gmail.com>
2024-09-18 18:30:31 -0300

    update RegistrarHoldingDTO.ts

--- src/useCases/registrarHolding/RegistrarHoldingDTO.ts
+++ src/useCases/registrarHolding/RegistrarHoldingDTO.ts
@@ -1,9 +1,8 @@
-import { EntidadeOperacional } from "../../domain/entities/EntidadeOperacional";
+import { EntidadeOperacional } from '../../domain/entities/EntidadeOperacional';
 
 export interface RegistrarHoldingDTO {
   id?: string;
   nome: string;
   entidadesOperacionais: EntidadeOperacional[];
   dataCriacao: Date;
-  }
-  
+}
