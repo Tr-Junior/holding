bbd1652e3d5ff9430fdc924d4cff4aba97f2ba4b
Trjunior <tarcisio.junior1406@gmail.com>
2024-09-20 17:17:08 -0300

    update IHoldingRepository.ts

--- src/domain/repositories/IHoldingRepository.ts
+++ src/domain/repositories/IHoldingRepository.ts
@@ -3,7 +3,7 @@ import { Holding } from '../entities/Holding';
 export interface IHoldingRepository {
   save(holding: Holding): Promise<Holding>;
   findById(id: string): Promise<Holding | null>;
-  // update(id: string, holdingData: Partial<Holding>): Promise<Holding | null>;
+  update(id: string, holdingData: Partial<Holding>): Promise<Holding | null>;
   findAll(): Promise<Holding[]>;
   // delete(id: string): Promise<void>;
 }
